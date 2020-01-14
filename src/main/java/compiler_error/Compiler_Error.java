80fa3e61f897e10bb3cb282e989813ec7f98a286
MinhPC <Needforspeed1900@gmail.com>
2020-01-14 11:58:05 +0700

    update

--- src/main/java/compiler_error/Compiler_Error.java
+++ src/main/java/compiler_error/Compiler_Error.java
@@ -7,17 +7,6 @@ import java.util.regex.Pattern;
 
 public class Compiler_Error {
     private JSONObject json_err_msg;
-    private Stack<String> error_stack;
-
-    // Chuyển array dòng lỗi thành stack
-    public Stack<String> toStack(String input) {
-        Pattern regex = Pattern.compile("\n", Pattern.MULTILINE);
-        String[] compile_error_arr = regex.split(input);
-        for (String line: compile_error_arr) {
-            error_stack.push(line);
-        }
-        return error_stack;
-    }
 
     public JSONObject Jsonify(String input) {
         Pattern regex = Pattern.compile("\n", Pattern.MULTILINE);
@@ -27,11 +16,12 @@ public class Compiler_Error {
         for (String line: compile_error_arr) {
             System.out.print(line + ", ");
             if (line.matches("(.*)^(symbol):(.*)")) {
-                String[] symbol = line.split("symbol:");
-                json_err_msg.put("symbol", symbol[0]);
+                json_err_msg.put("symbol", line.split("symbol:")[0].trim());
             }
-            else if ()
-            if (line.contains(":")) {
+            else if (line.matches("(.*)^(location):(.*)")) {
+                json_err_msg.put("className", line.split("location:")[0].trim());
+            }
+            else if (line.contains(":")) {
                 String[] line_array = line.split(":");
                 for (String split_string: line_array) {
                     // Truy vấn để kiểm tra
@@ -39,11 +29,12 @@ public class Compiler_Error {
                         json_err_msg.put("line", Integer.getInteger(split_string));
                     }
                     else if (split_string.matches("(.*)^(\\w+)\\.(java)$(.*)")) {
-                        json_err_msg.put("className", split_string.split(".java")[0]);
+                        json_err_msg.put("message", line_array[line_array.length - 1].trim());
                     }
 //                    else if (split_string.matches("^\\<[\\w \\W ]*\\> but was$")) {
 //
 //                    }
+                    
                 }
             }
         }
