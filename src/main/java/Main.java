1854dfd673b56b1d5a513c68a5f8acebf0d24271
MinhPC <Needforspeed1900@gmail.com>
2020-01-14 12:13:19 +0700

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
+import compiler_error.Compiler_Error;
+import org.json.JSONObject;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Compiler_Error error = new Compiler_Error();
+        String compiler_error_msg = "symbol: class Account\n" +
+                "location: class MyTest\n" +
+                "MyTest.java:25: error: cannot find symbol\n" +
+                "withdrawMethod = JavaReflection.getMethod(Account.class, withdraw, void.class, , ,";
+        JSONObject json_error = error.Jsonify(compiler_error_msg);
+        System.out.println(json_error.toString());
 
     }
 }
--- src/main/java/compiler_error/Compiler_Error.java
+++ src/main/java/compiler_error/Compiler_Error.java
@@ -2,31 +2,28 @@ package compiler_error;
 
 import org.json.JSONObject;
 
-import java.util.Stack;
 import java.util.regex.Pattern;
 
 public class Compiler_Error {
-    private JSONObject json_err_msg;
 
     public JSONObject Jsonify(String input) {
         Pattern regex = Pattern.compile("\n", Pattern.MULTILINE);
-
+        JSONObject json_err_msg = new JSONObject();
         String[] compile_error_arr = regex.split(input);
 
         for (String line: compile_error_arr) {
-            System.out.print(line + ", ");
             if (line.matches("(.*)^(symbol):(.*)")) {
-                json_err_msg.put("symbol", line.split("symbol:")[0].trim());
+                json_err_msg.put("symbol", line.split("symbol:")[1].trim());
             }
             else if (line.matches("(.*)^(location):(.*)")) {
-                json_err_msg.put("className", line.split("location:")[0].trim());
+                json_err_msg.put("className", line.split("location:")[1].trim());
             }
             else if (line.contains(":")) {
                 String[] line_array = line.split(":");
                 for (String split_string: line_array) {
                     // Truy vấn để kiểm tra
-                    if (split_string.matches("(.*)^[\\d]+$(.*)")) {
-                        json_err_msg.put("line", Integer.getInteger(split_string));
+                    if (split_string.trim().matches("(.*)^[\\d]+$(.*)")) {
+                        json_err_msg.put("line", split_string);
                     }
                     else if (split_string.matches("(.*)^(\\w+)\\.(java)$(.*)")) {
                         json_err_msg.put("message", line_array[line_array.length - 1].trim());
@@ -34,7 +31,7 @@ public class Compiler_Error {
 //                    else if (split_string.matches("^\\<[\\w \\W ]*\\> but was$")) {
 //
 //                    }
-                    
+
                 }
             }
         }
